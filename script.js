7128bbbc30bb95512f77caa7dd0da4c227aaa0f7
Abhishek Mishra <abhishekmishra65836583@gmail.com>
2025-05-29 16:41:50 +0530

    Javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+function addToCart() {
+    alert("Item added to Cart!");
+}
+window.onload = function () {
+    alert("welcome to our ONMKT site !!");
+};
+function changeText() {
+    document.getElementById("buybtn").innerHTML = "Added !";
+}
+function changeText1() {
+    document.getElementById("buybtn1").innerHTML = "Added !";
+}
+function changeText2() {
+    document.getElementById("buybtn2").innerHTML = "Added !";
+}
+function changeText3() {
+    document.getElementById("buybtn3").innerHTML = "Added !";
+}
+function changeText4() {
+    document.getElementById("buybtn4").innerHTML = "Added !";
+}
+function changeText5() {
+    document.getElementById("buybtn5").innerHTML = "Added !";
+}
+function changeText6() {
+    document.getElementById("buybtn6").innerHTML = "Added !";
+}
+function changeText7() {
+    document.getElementById("buybtn7").innerHTML = "Added !";
+}
+function changeText8() {
+    document.getElementById("buybtn8").innerHTML = "Added !";
+}
+function changeText9() {
+    document.getElementById("buybtn9").innerHTML = "Added !";
+}
+function changeText10() {
+    document.getElementById("buybtn10").innerHTML = "Added !";
+}
+function changeText0() {
+    document.getElementById("buybtn0").innerHTML = "Added !";
+}
+document.getElementById("date").innerText = " " + new Date().toDateString();
